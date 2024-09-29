584db7e0b6720f5e4f4531012b4b75ae43f6844f
JhonatanOro <jhowillsc@gmail.com>
2024-09-29 00:10:38 -0500

    Error Correguido

--- Controllers/CuentaController.cs
+++ Controllers/CuentaController.cs
@@ -11,37 +11,35 @@ using Microsoft.Extensions.Logging;
 
 namespace App_Bancaria.Controllers
 {
+  
     public class CuentaController : Controller
     {
         private readonly ILogger<CuentaController> _logger;
         private readonly ApplicationDbContext _context;
 
-        public CuentaController(ILogger<CuentaController> logger, ApplicationDbContext context)
+                public CuentaController(ILogger<CuentaController> logger, ApplicationDbContext context)
         {
             _logger = logger;
             _context = context;
         }
 
-        
         public IActionResult Index()
         {
-            var misCuentas = _context.Cuentas.ToList();
-            _logger.LogDebug("Cuentas: {misCuentas}", misCuentas);
-
-            var viewModel = new CuentaViewModel
+            var misCuentas= _context.Cuentas.ToList();
+            _logger.LogDebug("Cuentas:{misCuentas}",misCuentas);
+            var viewModel= new CuentaViewModel
             {
-                FormCuenta = new Cuenta(),
-                ListCuenta = misCuentas
+                FormCuenta= new Cuenta(),
+                ListCuenta= misCuentas
             };
-
             _logger.LogDebug("ViewModel: {viewModel}", viewModel);
             return View(viewModel);
         }
 
-        
         [HttpPost]
         public IActionResult Registrar(CuentaViewModel viewModel)
         {
+            
             var cuenta = new Cuenta
             {
                 NombreTitular = viewModel.FormCuenta.NombreTitular,
@@ -53,7 +51,7 @@ namespace App_Bancaria.Controllers
             _context.Cuentas.Add(cuenta);
             _context.SaveChanges();
 
-            ViewData["Message"] = "Cuenta Bancaria Registrada Exitosamente";
+            ViewData["Message"] = "Cuenta Bancaria Registrada";
 
             var misCuentas = _context.Cuentas.ToList();
             viewModel.ListCuenta = misCuentas;
@@ -61,17 +59,7 @@ namespace App_Bancaria.Controllers
             return View("Index", viewModel);
         }
 
-    
-        public IActionResult Listar()
-        {
-            var misCuentas = _context.Cuentas.ToList(); 
-            var viewModel = new CuentaViewModel
-            {
-                ListCuenta = misCuentas
-            };
 
-            return View(viewModel);  
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
